01ed9f752fcdb994a59290819c03034137eaf743
thiendo <thienmonkey1069@gmail.com>
2020-11-30 22:07:40 +0700

    add Login Activity

---
+++ app/src/main/java/com/example/demogithub/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.demogithub.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.demogithub.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
